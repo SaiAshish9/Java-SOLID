1da1e8c44b62a25ab0949de10f8149906e4a8327
SaiAshish <saiashish3760@gmail.com>
2023-10-08 12:06:02 +0530

    Add files via upload

---
+++ SOLID/src/main/java/org/sai/srp/TextPrinter.java
@@ -0,0 +1,21 @@
+package org.sai.srp;
+
+import org.sai.srp.TextManipulator;
+
+public class TextPrinter {
+
+    private TextManipulator textManipulator;
+
+    public TextPrinter(TextManipulator textManipulator) {
+        this.textManipulator = textManipulator;
+    }
+
+    public void printText() {
+        System.out.println(textManipulator.getText());
+    }
+
+    public void printCharactersOfText() {
+        System.out.println(textManipulator.getText().split(" "));
+    }
+
+}
